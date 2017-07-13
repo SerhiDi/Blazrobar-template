b98208d691bf21f425b698ad5cfa472e3e884b0a
Sergei Diachenko <traziusbiz@gmail.com>
2017-07-13 11:08:05 +0300

    add main slider functional

---
+++ js/image-slider.js
@@ -0,0 +1,142 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    var sliderActive = false;
+    var slider;
+    checkSlider();
+
+    window.addEventListener('resize', function () {
+      checkSlider();
+    });
+
+    function checkSlider() {
+      if (!sliderActive && window.innerWidth <= 640) {
+        slider = new Slider('.image-gallery');
+        sliderActive = true
+      } else if (sliderActive && window.innerWidth > 640) {
+        slider.destroy();
+        sliderActive = false;
+      }
+    }
+
+  });
+
+  function Slider(element) {
+    this.el = document.querySelector(element);
+    this.init();
+  }
+
+  Slider.prototype = {
+    //init
+    init: function () {
+      this.wrapper = this.el.querySelector('.images');
+      this.wrapper.style.left = 0;
+      this.el.classList.add('image-slider');
+      this.slides = this.wrapper.querySelectorAll('.image');
+      this.currentSlideIndex = 0;
+      this.createArrows();
+      this.createPagination();
+    },
+
+    //destroy
+    destroy: function () {
+      this.el.classList.remove('image-slider');
+      this.paginationBlock.parentNode.removeChild(this.paginationBlock);
+      this.arrowsBlock.parentNode.removeChild(this.arrowsBlock);
+    },
+
+    //pagination
+    createPagination: function () {
+      this.paginationBlock = document.createElement('div');
+      this.paginationBlock.classList.add('pagination');
+      for (var i = 0; i < this.slides.length; i++) {
+        var link = document.createElement('span');
+        link.className = 'link';
+        link.setAttribute('data-slide', i);
+        this.paginationBlock.appendChild(link);
+        this.paginationClick(link);
+      }
+      this.el.appendChild(this.paginationBlock);
+      this.links = this.paginationBlock.querySelectorAll('.link');
+      this.setCurrentSlide(this.currentSlideIndex);
+    },
+
+    //arrows
+    createArrows: function () {
+      this.arrowsBlock = document.createElement('div');
+      this.arrowsBlock.classList.add('arrows');
+      var slider = this;
+
+      function createArrow(className, text, clickEvent) {
+        var arrow = document.createElement('span');
+        arrow.classList.add(className);
+        arrow.innerHTML = text;
+        arrow.addEventListener('click', function () {
+          clickEvent(slider);
+        });
+        return arrow;
+      }
+
+      this.leftArrow = createArrow('arrow-left', '&#8249;', this.slidePrev);
+      this.rightArrow = createArrow('arrow-right', '&#8250;', this.slideNext);
+      this.arrowsBlock.appendChild(this.leftArrow);
+      this.arrowsBlock.appendChild(this.rightArrow);
+
+      this.el.appendChild(this.arrowsBlock);
+    },
+
+    slidePrev: function (slider) {
+      if (slider.currentSlideIndex > 0) {
+        slider.currentSlideIndex -= 1;
+        slider.setCurrentSlide(slider.currentSlideIndex);
+        slider.moveToSlide();
+      }
+    },
+
+    slideNext: function (slider) {
+      if (slider.currentSlideIndex < slider.slides.length - 1) {
+        slider.currentSlideIndex += 1;
+        slider.setCurrentSlide(slider.currentSlideIndex);
+        slider.moveToSlide();
+      }
+    },
+
+    setCurrentSlide: function (slideIndex) {
+      Array.prototype.forEach.call(this.links, function (link, i) {
+        if (+slideIndex === i) {
+          link.classList.add('active');
+        } else {
+          link.classList.remove('active');
+        }
+      });
+
+      slideIndex === 0 ?
+        this.leftArrow.classList.add('inactive') :
+        this.leftArrow.classList.remove('inactive');
+      slideIndex === this.slides.length - 1 ?
+        this.rightArrow.classList.add('inactive') :
+        this.rightArrow.classList.remove('inactive');
+    },
+
+    paginationClick: function (element) {
+      var self = this;
+      element.addEventListener('click', function (e) {
+        e.preventDefault();
+        var page = this;
+        self.currentSlideIndex = parseInt(page.getAttribute('data-slide'));
+        self.setCurrentSlide(self.currentSlideIndex);
+        self.moveToSlide();
+      }, false);
+    },
+
+    moveToSlide: function () {
+      var currentPage = this.el.querySelector('.image:nth-child(' + (this.currentSlideIndex + 1) + ')');
+      this.wrapper.style.left = '-' + currentPage.offsetLeft + 'px';
+    }
+
+  };
+
+})();
+
+
+
+
